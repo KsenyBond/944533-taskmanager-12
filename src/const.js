0e58c66f9c38bf9af6bf641393eee31ee73ebefa
KsenyBond <kseny.bondarchuk@gmail.com>
2020-08-25 02:21:49 +0300

    implement sort

--- src/const.js
+++ src/const.js
@@ -1,4 +1,9 @@
 const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
 const MAX_DAYS_GAP = 7;
+const SortType = {
+  DEFAULT: `default`,
+  DATE_DOWN: `date-down`,
+  DATE_UP: `date-up`
+};
 
-export {COLORS, MAX_DAYS_GAP};
+export {COLORS, MAX_DAYS_GAP, SortType};
